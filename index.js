4c34ca3b5b60e6af7f2ee65dd37ffcea66408f49
Dmitry <dm.rubashko@gmail.com>
2021-09-17 23:21:26 +0300

    + task_2

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // task_1
-const baseURL = 'https://pokeapi.co/api/v2/pokemon';
+const baseURL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
 const getPokemonsNames = async () => {
     const responce = await fetch(baseURL);
     const result = await responce.json();
-    console.log(result.results.map(item => item.name));
+    console.log(result.results.map(pokemon => pokemon.name));
 }
 
-getPokemonsNames()
+getPokemonsNames();
+
+// task_2
+let pokemonURL = 'https://pokeapi.co/api/v2/pokemon/';
+const getPokemonFullInfo = async (pokemonName) => {
+    pokemonURL += pokemonName;
+
+try {
+    const responce = await fetch(pokemonURL);
+    const result = await responce.json();
+    console.log(result);   
+} catch {
+    console.log (`Pokemon "${pokemonName}" not found!`)
+}
+}
+getPokemonFullInfo('pikachu'); // getPokemonFullInfo('Dima');
+
+// task_3
+
+const pokeapiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
