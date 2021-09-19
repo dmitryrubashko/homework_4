d47f2e6c6be359ac22d280510821f2b2933f1767
Dmitry <dm.rubashko@gmail.com>
2021-09-19 21:07:05 +0300

    + some new fixes

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // task_1
 const baseURL = 'https://pokeapi.co/api/v2/pokemon/?limit=1200';
 const getPokemonsNames = async () => {
-    const responce = await fetch(baseURL);
-    const result = await responce.json();
-    console.log(result.results.map(pokemon => pokemon.name))
+
+    try {
+        const responceFromPokeapi = await fetch(baseURL);
+        const resultOfRequestPokeapi = await responceFromPokeapi.json();
+        console.log(resultOfRequestPokeapi.results.map(pokemon => pokemon.name))
+    } catch (error) {
+        console.log ('Error', error)
+    } finally {
+        console.log ('Check console to know if you have problems with response from PokeApi or not')
+    }
+    
 }
 
 getPokemonsNames();
@@ -11,15 +19,17 @@ getPokemonsNames();
 // task_2
 let pokemonURL = 'https://pokeapi.co/api/v2/pokemon/';
 const getPokemonFullInfo = async (pokemonName) => {
-    pokemonURL += pokemonName
-
-try {
-    const responce = await fetch(pokemonURL);
-    const result = await responce.json();
-    console.log(result)
-} catch {
-    console.log (`Pokemon "${pokemonName}" not found!`)
-}
+    
+    try {
+        pokemonURL = pokemonURL+`${pokemonName}`;
+        const responceFromPokeapi = await fetch(pokemonURL);
+        const resultOfRequestPokeapi = await responceFromPokeapi.json();
+        console.log(resultOfRequestPokeapi)
+    } catch {
+        console.log (`Pokemon "${pokemonName}" not found!`)
+    } finally {
+        console.log ('Check console to know if you have problems with response from PokeApi or not')
+    }
 
 }
 
@@ -29,11 +39,12 @@ getPokemonFullInfo('pikachu'); // getPokemonFullInfo('Dima') // previous code wi
 
 const pokeapiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=99';
 const getfullInfoArray = async () => {
-    const responce = await fetch(pokeapiURL);
-    const result = await responce.json();
-    const UrlsArray = result.results.map(pokemon => pokemon.url);
-    const fullInfoArray = UrlsArray.map(url => fetch(url).then(pokemon => pokemon.json()));
+    
     try {
+        const responceFromPokeapi = await fetch(pokeapiURL);
+        const resultOfRequestPokeapi = await responceFromPokeapi.json();
+        const urlsArray = resultOfRequestPokeapi.results.map(pokemon => pokemon.url);
+        const fullInfoArray = urlsArray.map(url => fetch(url).then(pokemon => pokemon.json()));
         const responses = await Promise.all(fullInfoArray);
         console.log(responses)
     } catch (error) {
@@ -41,8 +52,7 @@ const getfullInfoArray = async () => {
     } finally {
         console.log (`This message means that you definitely added method "finally()" to your code! But not only that.`)
     }
+
 }
 
 getfullInfoArray()
-
-
